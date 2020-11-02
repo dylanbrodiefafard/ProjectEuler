b588395bbe0e61ebcea88d788ad4470f4a60c498
Dylan Fafard (dyf477) <dylan.fafard@usask.ca>
2020-11-01 19:35:12 -0600

    Optimize solution.

--- PythonProject/solutions/Solution14.py
+++ PythonProject/solutions/Solution14.py
@@ -7,33 +7,47 @@ class Solution14(SolutionBase):
     NUMBER = 14
     VERIFIED_ANSWER = 837799
 
-    def __init__(self):
-        self._chain_lengths = {}
-
     def run_tests(self, test_case):
-        test_case.assertEqual(10, self.get_chain_length(13))
+        chain_lengths = {1: 1}
+        test_case.assertEqual(10, self.chain_length(13, chain_lengths))
+        test_case.assertDictEqual({13: 10, 40: 9, 20: 8, 10: 7, 5: 6, 16: 5, 8: 4, 4: 3, 2: 2, 1: 1}, chain_lengths)
 
-    def get_chain_length(self, n):
+    @staticmethod
+    def chain_length(n: int, known_chain_lengths: dict):
         chain_length = 1
+        chain = [n]
         x = n
         while x != 1:
-            if x % 2 == 0:
-                x = x / 2
+            is_odd = x & 1
+            if is_odd:
+                # 3 * x + 1 is always even, so we can go ahead twice when x is odd.
+                x = (3 * x + 1) // 2
             else:
-                x = x * 3 + 1
-            if x in self._chain_lengths:
-                chain_length += self._chain_lengths[x]
+                x //= 2
+            if x in known_chain_lengths:
+                chain_length += known_chain_lengths[x]
                 break
-            chain_length += 1
-        self._chain_lengths[n] = chain_length
+            if is_odd:
+                # We skipped 2 steps when x was odd, so count them both here.
+                chain_length += 2
+                chain.extend((2 * x, x))
+            else:
+                chain_length += 1
+                chain.append(x)
+        # Record all of the chain lengths of the terms we encountered along the way.
+        for i, x in enumerate(chain):
+            known_chain_lengths[x] = chain_length - i
         return chain_length
 
     def get_answer(self):
         Answer = namedtuple('Answer', ['n', 'chain_length'])
         answer = Answer(None, 0)
-        self._chain_lengths.clear()
+        chain_lengths = {13: 10, 40: 9, 20: 8, 10: 7, 5: 6, 16: 5, 8: 4, 4: 3, 2: 2, 1: 1}
+        get_chain_length = self.chain_length
         for n in range(1, 1000000):
-            chain_length = self.get_chain_length(n)
+            if n in chain_lengths:
+                continue
+            chain_length = get_chain_length(n, chain_lengths)
             if chain_length > answer.chain_length:
                 answer = Answer(n, chain_length)
         return answer.n

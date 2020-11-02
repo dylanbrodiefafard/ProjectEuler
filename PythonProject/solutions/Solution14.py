8266bf6a16dbde47b334af2ad2c1ba946d40cab1
Dylan Fafard (dyf477) <dylan.fafard@usask.ca>
2020-11-02 12:18:09 -0600

    Simplify and optimize.

--- PythonProject/solutions/Solution14.py
+++ PythonProject/solutions/Solution14.py
@@ -14,27 +14,16 @@ class Solution14(SolutionBase):
 
     @staticmethod
     def chain_length(n: int, known_chain_lengths: dict):
-        chain_length = 1
         chain = [n]
-        x = n
-        while x != 1:
-            is_odd = x & 1
-            if is_odd:
-                # 3 * x + 1 is always even, so we can go ahead twice when x is odd.
-                x = (3 * x + 1) // 2
+        while n != 1:
+            if n & 1:
+                n = (3 * n + 1)
             else:
-                x //= 2
-            if x in known_chain_lengths:
-                chain_length += known_chain_lengths[x]
+                n //= 2
+            if n in known_chain_lengths:
                 break
-            if is_odd:
-                # We skipped 2 steps when x was odd, so count them both here.
-                chain_length += 2
-                chain.extend((2 * x, x))
-            else:
-                chain_length += 1
-                chain.append(x)
-        # Record all of the chain lengths of the terms we encountered along the way.
+            chain.append(n)
+        chain_length = len(chain) + known_chain_lengths.get(n, 0)
         for i, x in enumerate(chain):
             known_chain_lengths[x] = chain_length - i
         return chain_length
@@ -44,11 +33,11 @@ class Solution14(SolutionBase):
         answer = Answer(None, 0)
         chain_lengths = {13: 10, 40: 9, 20: 8, 10: 7, 5: 6, 16: 5, 8: 4, 4: 3, 2: 2, 1: 1}
         get_chain_length = self.chain_length
-        for n in range(1, 1000000):
+        # skip even numbers because they have shorter chains than odd numbers.
+        for n in range(13, 1000000, 2):
             if n in chain_lengths:
                 continue
-            chain_length = get_chain_length(n, chain_lengths)
-            if chain_length > answer.chain_length:
+            if (chain_length := get_chain_length(n, chain_lengths)) > answer.chain_length:
                 answer = Answer(n, chain_length)
         return answer.n
 
